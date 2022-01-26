03618a819162089838e247f3091bf4d7c61b1acd
enoch harnanto <enochharnantoo@gmail.com>
2022-01-25 18:50:13 -0800

    Added hover functionality to graph for one color

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const container = document.getElementById('container');
+const clear = document.getElementById('clear');
+const draw = document.getElementById('black');
+const rainbowDraw = document.getElementById('rainbow');
+
+const greenHover = 'green-hover-style';
+
+
+
 
 function createGrid(rows, cols) {
+    // For reference on grid :https://fionnachan.medium.com/dynamic-number-of-rows-and-columns-with-css-grid-layout-and-css-variables-cb8e8381b6f2
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     const size = rows * cols;
     for (i = 0; i < size; i++) {
         const cell = document.createElement('div');
         cell.classList.add("cell");
+        cell.addEventListener('mouseover', () => {
+            cell.classList.add(greenHover);
+        });
         container.appendChild(cell).className = "grid-item";
     }
 }
 
-createGrid(64, 64);
+function clearGrid() {
+    // For reference on grid :https://fionnachan.medium.com/dynamic-number-of-rows-and-columns-with-css-grid-layout-and-css-variables-cb8e8381b6f2
+    const greenHover = 'green-hover-style';
+    const cellList = container.querySelectorAll('div');
+    cellList.forEach(function (element) {
+        element.classList.remove(greenHover);
+    })
+}
+
+clear.addEventListener("click", clearGrid);
+createGrid(64,64);
+
+
