e303b245873cbf0bf418ff6cbbf892f905801eea
enoch harnanto <enochharnantoo@gmail.com>
2022-01-25 19:03:27 -0800

    Figured out how to do multiple colors and clear the grid

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const draw = document.getElementById('black');
 const rainbowDraw = document.getElementById('rainbow');
 
 const greenHover = 'green-hover-style';
+const blackHover = 'black-hover-style';
+let hover = "";
+
+draw.addEventListener("click", () => {
+    hover = blackHover;
+});
+
+rainbowDraw.addEventListener("click", () => {
+    hover = greenHover;
+})
+
 
 
 
@@ -17,7 +28,7 @@ function createGrid(rows, cols) {
         const cell = document.createElement('div');
         cell.classList.add("cell");
         cell.addEventListener('mouseover', () => {
-            cell.classList.add(greenHover);
+            cell.classList.add(hover);
         });
         container.appendChild(cell).className = "grid-item";
     }
@@ -25,10 +36,10 @@ function createGrid(rows, cols) {
 
 function clearGrid() {
     // For reference on grid :https://fionnachan.medium.com/dynamic-number-of-rows-and-columns-with-css-grid-layout-and-css-variables-cb8e8381b6f2
-    const greenHover = 'green-hover-style';
     const cellList = container.querySelectorAll('div');
+    const classes = [greenHover, blackHover];
     cellList.forEach(function (element) {
-        element.classList.remove(greenHover);
+        element.classList.remove(...classes);
     })
 }
 
