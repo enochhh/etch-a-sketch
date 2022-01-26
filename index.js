b7e9444c55d2fb7c72d83698466da24656f1529b
enoch harnanto <enochharnantoo@gmail.com>
2022-01-26 15:20:36 -0800

    Restructured code so that color setting wasn't done during grid creation; added 'clear' and 'erase' functionality

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const container = document.getElementById('container');
 const clear = document.getElementById('clear');
-const draw = document.getElementById('black');
+const erase = document.getElementById('erase');
+const blackDraw = document.getElementById('black');
 const rainbowDraw = document.getElementById('rainbow');
 
-const greenHover = 'green-hover-style';
-const blackHover = 'black-hover-style';
-let hover = "";
-
-draw.addEventListener("click", () => {
-    hover = blackHover;
-});
-
-rainbowDraw.addEventListener("click", () => {
-    hover = greenHover;
-})
-
-
-
-
+clear.addEventListener("click", clearGrid);
+blackDraw.addEventListener("click", colorItem);
+rainbowDraw.addEventListener("click", colorItem);
+erase.addEventListener("click", eraseGrid);
 
 function createGrid(rows, cols) {
     // For reference on grid :https://fionnachan.medium.com/dynamic-number-of-rows-and-columns-with-css-grid-layout-and-css-variables-cb8e8381b6f2
@@ -27,23 +17,63 @@ function createGrid(rows, cols) {
     for (i = 0; i < size; i++) {
         const cell = document.createElement('div');
         cell.classList.add("cell");
-        cell.addEventListener('mouseover', () => {
-            cell.classList.add(hover);
-        });
+        // cell.addEventListener('mouseover', () => {
+        //     cell.classList.add(hover);
+        // });
         container.appendChild(cell).className = "grid-item";
     }
 }
 
-function clearGrid() {
+function eraseGrid() {
     // For reference on grid :https://fionnachan.medium.com/dynamic-number-of-rows-and-columns-with-css-grid-layout-and-css-variables-cb8e8381b6f2
-    const cellList = container.querySelectorAll('div');
-    const classes = [greenHover, blackHover];
-    cellList.forEach(function (element) {
-        element.classList.remove(...classes);
+    let gridItem = document.querySelectorAll(".grid-item");
+    gridItem.forEach(item => {
+        item.addEventListener('mouseover', (e) => {
+            resetColor(item);
+        })
     })
 }
 
-clear.addEventListener("click", clearGrid);
+function clearGrid() {
+    let gridItem = document.querySelectorAll(".grid-item");
+    gridItem.forEach(item => {
+        resetColor(item);
+    })
+}
+
+/* Rainbow color */
+function random (number) {
+    return Math.floor(Math.random() * (number+1));
+}
+  
+function rainbowColor (e) {
+    const rndCol= 'rgb(' + random(255) + ',' + random(255) +',' + random(255) + ')';
+    e.style.backgroundColor = rndCol;
+    //console.log(e.target);
+    /*Wondering why 'e.target.style' kept returning undefined; found that it was due to the fact that 'e' was not an existing target 
+    at the time of function creation*/
+}
+
+/* Default color (black) */
+function blackColor (e) {
+    e.style.backgroundColor = 'black';
+}
+
+function resetColor (e) {
+    e.style.backgroundColor = 'transparent';
+}
+
+
+
+/*Color select of each grid square*/
+function colorItem() {
+    let gridItem = document.querySelectorAll(".grid-item");
+    gridItem.forEach(item => item.addEventListener('mouseover', (e) => {
+        rainbowColor(item);
+    })
+    );
+} 
+
 createGrid(64,64);
 
 
